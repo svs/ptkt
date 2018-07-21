603d4a46688269dd3e8ef97db390b228b6781e62
Akshay Nair <phenax5@gmail.com>
2018-07-21 17:14:58 +0530

    Fixes directory structure

--- src/test/kotlin/com/shaadi/ProfileTest.kt
+++ src/test/kotlin/com/shaadi/ProfileTest.kt
@@ -3,9 +3,8 @@ package com.shaadi.profile
 import io.kotlintest.specs.ShouldSpec
 import io.kotlintest.*
 
-class ProfileTests : StringSpec({
+class ProfileTest : StringSpec({
     "act should be done" {
         ShaadiProfile().act() shouldBe "DONE"
     }
-
 }
